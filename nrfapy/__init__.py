e91d70300c9ae46e83e50dd52127950a21e971c2
dngoldberg <31406058+dngoldberg@users.noreply.github.com>
2024-02-23 09:57:07 +0000

    udpate to version 0.5, add name to __init__.py

--- nrfapy/__init__.py
+++ nrfapy/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for nrfapy."""
 
-__author__ = """Simon M Mudd and Simon Moulds"""
+__author__ = """Simon M Mudd, Simon Moulds and Daniel N Goldberg"""
 __email__ = 'simon.m.mudd@ed.ac.uk'
-__version__ = '0.4'
+__version__ = '0.5'
 
 
 from .nrfapy import *
